55784ee4c127ba91f791eda84da7b3aa2646dd7d
V. Kukuyashnyy <starsonic71@gmail.com>
2021-04-20 23:34:40 +0300

    add phone in registration

--- src/main/java/org/itstep/telegrambot/handler/RegisterHandler.java
+++ src/main/java/org/itstep/telegrambot/handler/RegisterHandler.java
@@ -6,7 +6,7 @@ import org.itstep.telegrambot.Bot;
 import org.itstep.telegrambot.command.ParsedCommand;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
-public class RegisterHandler extends AbstractHandler{
+public class RegisterHandler extends AbstractHandler {
 
     private static final Logger log = Logger.getLogger(RegisterUserHandler.class);
 
@@ -16,15 +16,24 @@ public class RegisterHandler extends AbstractHandler{
 
     @Override
     public String operate(String chatId, ParsedCommand parsedCommand, Update update) {
-        if (!bot.userDao.isExist(update.getMessage().getFrom().getId())) {
-            User user = new User();
-            user.setUserId(update.getMessage().getFrom().getId());
-            user.setUserName(update.getMessage().getFrom().getUserName());
-            user.setFirstName(update.getMessage().getFrom().getFirstName());
-            user.setLastName(update.getMessage().getFrom().getLastName());
-
-            bot.userDao.save(user);
-            return "Вы зарегистрированы. Ожидайте подтверждения администратора.";
+        User user = bot.userDao.findUserById(update.getMessage().getFrom().getId());
+        if (user == null) {
+                try {
+                    user = new User();
+                    user.setUserId(update.getMessage().getFrom().getId());
+                    user.setUserName(update.getMessage().getFrom().getUserName());
+                    user.setFirstName(update.getMessage().getFrom().getFirstName());
+                    user.setLastName(update.getMessage().getFrom().getLastName());
+                    user.setPhone(update.getMessage().getContact().getPhoneNumber());
+                    bot.userDao.save(user);
+                    return "Вы зарегистрированы. Ожидайте подтверждения администратора.";
+                } catch (Exception e) {
+                    log.error(e.getMessage(), e);
+                    return "Извините, произошла ошибка. Повторите попытку позже.";
+                }
+            }
+        if (user.isUser() || user.isAdmin() || user.isMaster()) {
+            return "Вы уже зарегистрированы.";
         } else {
             return "Вы уже зарегистрированы, но администратор не потвердил вашу регистрацию.";
         }
